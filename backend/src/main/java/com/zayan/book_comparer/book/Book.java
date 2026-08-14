6b4b8d432bdd2e9f55e03e2f58256f29b219f60f
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-13 22:31:31 -0500

    rename to class to Book; add columns as member variables; generate constructors, getters, and setters

--- backend/src/main/java/com/zayan/book_comparer/book/Book.java
+++ backend/src/main/java/com/zayan/book_comparer/book/Book.java
@@ -1,4 +1,73 @@
 package com.zayan.book_comparer.book;
 
-public class BookEntity {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="books")
+public class Book {
+
+     @Id
+     private Long id;
+
+     private String title;
+
+     private String author;
+
+     @Column(columnDefinition = "TEXT")
+     private String text;
+
+     @Lob
+     @Column(columnDefinition = "BYTEA")
+     private byte[] image;
+
+     public Book() {
+     }
+
+     public Book(Long id, String title, String author, String text, byte[] image) {
+          this.id = id;
+          this.title = title;
+          this.author = author;
+          this.text = text;
+          this.image = image;
+     }
+
+     public Long getId() {
+          return id;
+     }
+
+     public void setId(Long id) {
+          this.id = id;
+     }
+
+     public String getTitle() {
+          return title;
+     }
+
+     public void setTitle(String title) {
+          this.title = title;
+     }
+
+     public String getAuthor() {
+          return author;
+     }
+
+     public void setAuthor(String author) {
+          this.author = author;
+     }
+
+     public String getText() {
+          return text;
+     }
+
+     public void setText(String text) {
+          this.text = text;
+     }
+
+     public byte[] getImage() {
+          return image;
+     }
+
+     public void setImage(byte[] image) {
+          this.image = image;
+     }
 }
