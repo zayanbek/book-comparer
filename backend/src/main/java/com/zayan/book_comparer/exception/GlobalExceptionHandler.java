d5624a6cd2919bd276b75d1ca93a4ab6cfa93711
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-14 18:22:39 -0500

    add GlobalExceptionHandler for handling BookNotFoundException

---
+++ backend/src/main/java/com/zayan/book_comparer/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.zayan.book_comparer.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+     @ExceptionHandler(BookNotFoundException.class)
+     public ResponseEntity<Map<String, String>> handleInvalidTeacherName(
+             BookNotFoundException ex) {
+
+          return ResponseEntity
+                  .status(HttpStatus.BAD_REQUEST)
+                  .body(Map.of(
+                          "error", "Bad Request",
+                          "message", ex.getMessage()
+                  ));
+     }
+}
