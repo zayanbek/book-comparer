19bac72da2bbfb631f8ce4d759f98e56ef101f46
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-13 21:36:57 -0500

    initial commit

---
+++ backend/src/main/java/com/zayan/book_comparer/BookComparerApplication.java
@@ -0,0 +1,15 @@
+package com.zayan.book_comparer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookComparerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BookComparerApplication.class, args);
+
+	}
+
+}
---
+++ backend/src/test/java/com/zayan/book_comparer/BookComparerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zayan.book_comparer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookComparerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
