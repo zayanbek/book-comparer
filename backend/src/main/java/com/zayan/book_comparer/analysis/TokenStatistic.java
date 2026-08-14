5f25c4233214f83eed2e78a1f08d8c5b1a3a03a3
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-14 18:57:22 -0500

    rename TokenStatistics class to TokenStatistic and implement frequency and probability attributes with their getters and setters

--- backend/src/main/java/com/zayan/book_comparer/analysis/TokenStatistic.java
+++ backend/src/main/java/com/zayan/book_comparer/analysis/TokenStatistic.java
@@ -1,6 +1,28 @@
 package com.zayan.book_comparer.analysis;
 
-public class TokenStatistics {
+public class TokenStatistic {
 
-     
+     private int frequency;
+     private double probability;
+
+     public TokenStatistic(int frequency, double probability) {
+          this.frequency = frequency;
+          this.probability = probability;
+     }
+
+     public int getFrequency() {
+          return frequency;
+     }
+
+     public void setFrequency(int frequency) {
+          this.frequency = frequency;
+     }
+
+     public double getProbability() {
+          return probability;
+     }
+
+     public void setProbability(double probability) {
+          this.probability = probability;
+     }
 }
