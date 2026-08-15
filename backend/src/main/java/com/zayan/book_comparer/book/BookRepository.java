309a56a9cc8142fbb2428f8771d175aa6961031f
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-14 23:05:44 -0500

    add findByTitle method to BookRepository for title-based book retrieval

--- backend/src/main/java/com/zayan/book_comparer/book/BookRepository.java
+++ backend/src/main/java/com/zayan/book_comparer/book/BookRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long>,
-        JpaSpecificationExecutor<Book>
-{}
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+     Optional<Book> findByTitle(String title);
+
+
+}
