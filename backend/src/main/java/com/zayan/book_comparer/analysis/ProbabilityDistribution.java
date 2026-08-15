64629ada1f60ed1b5af2a9aa0ec05bddf3d44ac5
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-14 19:11:13 -0500

    implement KL divergence

--- backend/src/main/java/com/zayan/book_comparer/analysis/ProbabilityDistribution.java
+++ backend/src/main/java/com/zayan/book_comparer/analysis/ProbabilityDistribution.java
@@ -5,26 +5,51 @@ import java.util.Map;
 public class ProbabilityDistribution {
 
      Map<String, TokenStatistic> distribution;
+     private static final double EPSILON = 1e-10;
 
      public ProbabilityDistribution(Map<String, TokenStatistic> distribution) {
           this.distribution = distribution;
      }
 
      public double entropy() {
-          double sum = 0;
+          double entropy = 0;
 
           for(TokenStatistic value : distribution.values()) {
                double probability = value.getProbability();
-               sum += probability * this.log2(probability);
+               entropy += probability * log2(probability);
           }
 
-          return -1 * sum;
+          return -1 * entropy;
      }
 
+     public double klDivergence(ProbabilityDistribution otherDistribution) {
+          double divergence = 0.0;
+
+          for (String token : distribution.keySet()) {
+
+               double p = distribution.get(token).getProbability();
+               double q = otherDistribution.getProbability(token);
+
+               if (p < EPSILON) continue;
+               if (q < EPSILON) return Double.POSITIVE_INFINITY;
+
+               divergence += p * log2(p / q);
+          }
+
+          return divergence;
+     }
+
+     public double jsDivergence(ProbabilityDistribution otherDistribution) {
+
+
+     }
+
+     public double cosineSimilarity(ProbabilityDistribution otherDistribution) {
 
 
 
-
+          return -3.14;
+     }
 
 
      private static double log2(double x) {
@@ -37,5 +62,15 @@ public class ProbabilityDistribution {
 
      public void setDistribution(Map<String, TokenStatistic> distribution) {
           this.distribution = distribution;
+     }
+
+     public double getProbability(String token) {
+          TokenStatistic statistic = distribution.get(token);
+
+          if (statistic == null) {
+               return 0.0;
+          }
+
+          return statistic.getProbability();
      }
 }
