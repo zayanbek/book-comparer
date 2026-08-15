b4891f0ecf008fcf1ef6cda7ac8431b17a59f57e
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-15 14:40:14 -0500

    refactor BookControllerTest to rename methods for clarity and update book retrieval logic

--- backend/src/test/java/com/zayan/book_comparer/book/BookControllerTest.java
+++ backend/src/test/java/com/zayan/book_comparer/book/BookControllerTest.java
@@ -23,7 +23,7 @@ class BookControllerTest {
      private BookService bookService;
 
      @Test
-     void getBook_shouldReturnCorrectBook() throws Exception {
+     void getBookById_shouldReturnCorrectBook() throws Exception {
 
           BookDto book = new BookDto();
           book.setId(1L);
@@ -33,15 +33,35 @@ class BookControllerTest {
           when(bookService.getBook(1L))
                   .thenReturn(book);
 
-          mockMvc.perform(
-                          get("/books")
-                                  .param("id", "1")
-                  )
+          mockMvc.perform(get("/books/1"))
                   .andExpect(status().isOk())
                   .andExpect(jsonPath("$.id").value(1))
                   .andExpect(jsonPath("$.title").value("The Odyssey"))
                   .andExpect(jsonPath("$.author").value("Homer"));
 
           verify(bookService).getBook(1L);
+     }
+
+     @Test
+     void getBookByTitle_shouldReturnCorrectBook() throws Exception {
+
+          Book book = new Book();
+          book.setId(1L);
+          book.setTitle("The Odyssey");
+          book.setAuthor("Homer");
+
+          when(bookService.getBook("The Odyssey"))
+                  .thenReturn(book);
+
+          mockMvc.perform(
+                          get("/books")
+                                  .param("title", "The Odyssey")
+                  )
+                  .andExpect(status().isOk())
+                  .andExpect(jsonPath("$.id").value(1))
+                  .andExpect(jsonPath("$.title").value("The Odyssey"))
+                  .andExpect(jsonPath("$.author").value("Homer"));
+
+          verify(bookService).getBook("The Odyssey");
      }
 }
