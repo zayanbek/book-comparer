06803ec540416177875a74bf1eb02ef28662893d
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-15 14:37:41 -0500

    add new constructor and member variable totalLength to TokenStatistic

--- backend/src/main/java/com/zayan/book_comparer/analysis/TokenStatistic.java
+++ backend/src/main/java/com/zayan/book_comparer/analysis/TokenStatistic.java
@@ -4,10 +4,22 @@ public class TokenStatistic {
 
      private int frequency;
      private double probability;
+     private int totalLength;
 
      public TokenStatistic(int frequency, double probability) {
           this.frequency = frequency;
           this.probability = probability;
+     }
+
+     public TokenStatistic(int totalLength) {
+          this.frequency = 0;
+          this.probability = 0;
+          this.totalLength = totalLength;
+     }
+
+     public void increment() {
+          frequency += 1;
+          probability = frequency / totalLength;
      }
 
      public int getFrequency() {
