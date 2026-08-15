2ff55d92d89170da3913c6fdd03cacda221cbb94
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-15 14:38:41 -0500

    change endpoint for getBookById and create new method getBookByTitle

--- backend/src/main/java/com/zayan/book_comparer/book/BookController.java
+++ backend/src/main/java/com/zayan/book_comparer/book/BookController.java
@@ -1,9 +1,6 @@
 package com.zayan.book_comparer.book;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/books")
@@ -15,8 +12,13 @@ public class BookController {
           this.bookService = bookService;
      }
 
-     @GetMapping
-     public BookDto getBook(@RequestParam(required = true) Long id) {
+     @GetMapping("/{id}")
+     public BookDto getBookById(@PathVariable Long id) {
           return bookService.getBook(id);
+     }
+
+     @GetMapping
+     public BookDto getBookByTitle(@RequestParam String title) {
+          return new BookDto(bookService.getBook(title));
      }
 }
