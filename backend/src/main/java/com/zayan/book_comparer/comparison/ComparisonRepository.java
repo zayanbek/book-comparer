eb2109b464d42e9210b52ac6e9ac8dd06464cf8d
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-14 23:06:18 -0500

    refactor ComparisonRepository to extend JpaRepository and add findByBookAIdAndBookBId method

--- backend/src/main/java/com/zayan/book_comparer/comparison/ComparisonRepository.java
+++ backend/src/main/java/com/zayan/book_comparer/comparison/ComparisonRepository.java
@@ -1,4 +1,12 @@
 package com.zayan.book_comparer.comparison;
 
-public interface ComparisonRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ComparisonRepository extends JpaRepository<Comparison, Long> {
+     Optional<Comparison> findByBookAIdAndBookBId(Long bookAId, Long bookBId);
 }
