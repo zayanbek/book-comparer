e71586d37e30a4e5908983e9387fcb705407ebdb
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-15 14:38:52 -0500

    add methods to find books by title with case insensitivity

--- backend/src/main/java/com/zayan/book_comparer/book/BookRepository.java
+++ backend/src/main/java/com/zayan/book_comparer/book/BookRepository.java
@@ -4,12 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
      Optional<Book> findByTitle(String title);
-
-
+     List<Book> findByTitleContainingIgnoreCase(String title);
+     Optional<Book> findByTitleIgnoreCase(String title);
 }
