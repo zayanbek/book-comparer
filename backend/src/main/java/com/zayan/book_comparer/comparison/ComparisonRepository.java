ac5b9e44f65f3dca62b9e1c590e3721f037eb0bf
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-15 22:51:06 -0500

    add  blank lines to ComparisonRepository

--- backend/src/main/java/com/zayan/book_comparer/comparison/ComparisonRepository.java
+++ backend/src/main/java/com/zayan/book_comparer/comparison/ComparisonRepository.java
@@ -8,5 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface ComparisonRepository extends JpaRepository<Comparison, Long> {
+
      Optional<Comparison> findByBookAIdAndBookBId(Long bookAId, Long bookBId);
+
 }
