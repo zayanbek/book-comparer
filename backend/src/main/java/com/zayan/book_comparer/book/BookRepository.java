ef340c88df38981ef2458cc7069ec248c45013f1
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-16 22:21:19 -0500

    remove unused import in BookRepository

--- backend/src/main/java/com/zayan/book_comparer/book/BookRepository.java
+++ backend/src/main/java/com/zayan/book_comparer/book/BookRepository.java
@@ -1,7 +1,6 @@
 package com.zayan.book_comparer.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
