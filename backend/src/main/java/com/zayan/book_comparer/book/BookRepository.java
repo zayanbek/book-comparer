123b660d9973b758c646f10311838425efd0932c
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-18 16:42:26 -0500

    add pagination based method to repository

--- backend/src/main/java/com/zayan/book_comparer/book/BookRepository.java
+++ backend/src/main/java/com/zayan/book_comparer/book/BookRepository.java
@@ -1,5 +1,7 @@
 package com.zayan.book_comparer.book;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +11,8 @@ import java.util.Optional;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-     Optional<Book> findByTitle(String title);
+     // Optional<Book> findByTitle(String title);
      List<Book> findByTitleContainingIgnoreCase(String title);
      Optional<Book> findByTitleIgnoreCase(String title);
+     Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 }
