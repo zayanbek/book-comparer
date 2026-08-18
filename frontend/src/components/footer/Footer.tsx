4e0f207971d99e04a5c3a2054663da4f39ad2a91
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-17 20:45:19 -0500

    create and implement footer component

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>Book Comparer</p>
+      </div>
+    </footer>
+  );
+}
