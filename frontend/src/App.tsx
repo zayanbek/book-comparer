dc9f2494e9e1edeb4779d5908faaeabf9552e2f6
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-17 20:44:45 -0500

    add search bar and plan next few components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,30 @@
 import "./App.css";
 
+import { useState } from "react";
+
 import Header from "./components/header/Header";
+import SearchBar from "./components/searchBar/SearchBar";
+import Footer from "./components/footer/Footer";
 
 export default function App() {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="app">
       <Header />
+
+      <main className="main">
+        <SearchBar value={search} onChange={setSearch} />
+        {/*
+        
+        <Carousel />
+        <Comparison />
+        <Metrics />
+
+        */}
+      </main>
+
+      <Footer />
     </div>
   );
 }
