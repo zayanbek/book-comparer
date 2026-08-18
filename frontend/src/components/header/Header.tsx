082020c1384833f76e7817792f9ff63cfbb1cc85
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-17 19:04:20 -0500

    add Header component with styling

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-content">
+        <h1 className="text">Book Comparer</h1>
+        <p>Measure divergence between two volumes</p>
+      </div>
+    </header>
+  );
+}
