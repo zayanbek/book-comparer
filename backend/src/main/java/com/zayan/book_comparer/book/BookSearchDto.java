6dc7ad9fa0de95a5c020bcf866250e9058fa6170
Zayan Bekinalkar <zayan_ahmad@hotmail.com>
2026-08-19 19:32:33 -0500

    Create alternative to BookDto; same dto, but without the text attribute

--- backend/src/main/java/com/zayan/book_comparer/book/BookSearchDto.java
+++ backend/src/main/java/com/zayan/book_comparer/book/BookSearchDto.java
@@ -1,4 +1,50 @@
 package com.zayan.book_comparer.book;
 
 public class BookSearchDto {
+
+     private Long id;
+     private String title, author;
+     private byte[] image;
+
+     public BookSearchDto() {
+     }
+
+     public BookSearchDto(Long id, String title, String author, byte[] image) {
+          this.id = id;
+          this.title = title;
+          this.author = author;
+          this.image = image;
+     }
+
+     public Long getId() {
+          return id;
+     }
+
+     public void setId(Long id) {
+          this.id = id;
+     }
+
+     public String getTitle() {
+          return title;
+     }
+
+     public void setTitle(String title) {
+          this.title = title;
+     }
+
+     public String getAuthor() {
+          return author;
+     }
+
+     public void setAuthor(String author) {
+          this.author = author;
+     }
+
+     public byte[] getImage() {
+          return image;
+     }
+
+     public void setImage(byte[] image) {
+          this.image = image;
+     }
 }
